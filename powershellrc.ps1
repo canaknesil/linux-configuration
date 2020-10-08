7c972ae0bd355c41a487f18f55ea8f24a1c6d7c1
Can Aknesil <aknesil@kth.se>
2020-10-09 00:15:31 +0200

    Powershellrc and emacs mode.

---
+++ powershellrc.ps1
@@ -0,0 +1,32 @@
+#
+# Use this script as such:
+#
+# $linux_configuration_proj_dir = "c:\Users\canaknesil\StandalonePrograms\linux-configuration"
+# $python_venv_dir = "c:\Users\canaknesil\.venv"
+# . "$linux_configuration_proj_dir\powershellrc.ps1"
+#
+
+if ($linux_configuration_proj_dir -ne $null) {
+    #export PATH=$PATH:$LINUX_CONFIGURATION_PROJ_DIR/bin
+}
+
+function e { emacs -nw @args } # The theme is horrible. 
+#function matlab { matlab -nodisplay @args } # This does not work.
+#function octave { octave-cli @args } # I don't have octave yet. 
+#function julia { julia --color=yes @args } # Somehow does not work.
+function l { ls @args }
+function py { python @args }
+function ipy { ipython @args }
+# Can't find anythink to send files to trash.
+
+function desk { pushd "c:\Users\$env:USERNAME\Desktop" }
+function down { pushd "d:\Users\$env:USERNAME\Downloads" }
+function homec { pushd "c:\Users\$env:USERNAME" }
+function homed { pushd "d:\Users\$env:USERNAME" }
+
+if ($python_venv_dir -ne $null) {
+    function venv-activate {
+	param($env_name)
+	. "$python_venv_dir\$env_name\Scripts\activate.ps1"
+    }
+}
